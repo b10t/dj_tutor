ff6de52fae85584e80087e547df34d1bf54b297f
b10t <b10t.rus@gmail.com>
2019-04-06 18:54:04 +0800

    Понятное отображение полей bboard в админке.

--- bboard/admin.py
+++ bboard/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Bb
 
-admin.site.register(Bb)
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_link = ('title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(Bb, BbAdmin)
--- bboard/views.py
+++ bboard/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from .models import Bb
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
 
     return render(request, 'bboard/index.html', {'bbs': bbs})
