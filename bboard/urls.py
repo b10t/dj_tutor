f8ed0a9c925e7e60bf3a21f235bca18c6b530dcf
b10t <b10t.rus@gmail.com>
2019-04-08 10:56:51 +0800

    Глава 2.5

--- bboard/urls.py
+++ bboard/urls.py
@@ -3,5 +3,5 @@ from .views import index, by_rubric
 
 urlpatterns = [
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('', index, name='name'),
+    path('', index, name='index'),
 ]
